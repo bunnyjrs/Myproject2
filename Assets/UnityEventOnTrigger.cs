8251da1df5ccc269b46b5db49d48562422511433
bunnyjr <149501501+bunnyjrs@users.noreply.github.com>
2023-11-09 14:07:44 +0100

    acid pool

--- Assets/UnityEventOnTrigger.cs
+++ Assets/UnityEventOnTrigger.cs
@@ -9,8 +9,6 @@ public class UnityEventOnTrigger : MonoBehaviour
     public UnityEvent onTriggerEnter, onTriggerExit;
 
     public string tagToActivate = "Player";
-    public string tagToActivate2 = "Key";
-
     private void Awake()
     {
         if ((GetComponent<Collider>() == null) && (GetComponent<Collider2D>() == null))
@@ -46,26 +44,11 @@ public class UnityEventOnTrigger : MonoBehaviour
             onTriggerEnter.Invoke();
             Debug.Log("Unity Event Trigger (enter) activated on " + gameObject);
         }
-
-        Debug.Log($"Other tag: {other.tag}");
-        Debug.Log($"Active: {tagToActivate2}");
-        Debug.Log($"Other is: {other}");
-        if (other.CompareTag(tagToActivate2))
-        {
-            onTriggerEnter.Invoke();
-            Debug.Log("Unity Event Trigger (enter) activated on " + gameObject);
-        }
     }
     
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag(tagToActivate))
-        {
-            onTriggerExit.Invoke();
-            Debug.Log("Unity Event Trigger (exit) activated on " + gameObject);
-        }
-
-        if (other.CompareTag(tagToActivate2))
         {
             onTriggerExit.Invoke();
             Debug.Log("Unity Event Trigger (exit) activated on " + gameObject);
